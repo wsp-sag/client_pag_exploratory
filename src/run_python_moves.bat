d82641fa0cc10ddb3b44c8dae38356a33b1a3948
Arash <39972246+arashasadabadi@users.noreply.github.com>
2024-03-01 10:54:21 -0800

    Create run_python_moves.bat
    Runs the python scripts for MOVES

---
+++ src/run_python_moves.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set PROJECT_PATH=D:/PAG/client_pag_exploratory
+
+set PYTHON_PATH=C:/Users/USAA670342/AppData/Local/anaconda3/python.exe
+
+cd /d %PROJECT_PATH%/src
+
+%PYTHON_PATH% %PROJECT_PATH%/src/MOVES_input1.py
+
+pause
