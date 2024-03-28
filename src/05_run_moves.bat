89ad5ededcb29a3c116b3c111019a60d1f44e424
Arash <39972246+arashasadabadi@users.noreply.github.com>
2024-03-28 16:08:30 -0700

    update batch file for running MOVES
    R scripts now use arguments in the batch file instead of reading the parameters from a config file. All the parameters are now set in this batch file and passed as arguments to R scripts

--- src/05_run_moves.bat
+++ src/05_run_moves.bat
@@ -2,28 +2,36 @@ rem Set the path to the Rscript executable
 set RSCRIPT="C:\Program Files\R\R-4.0.5\bin\Rscript.exe"
 
 rem Set the path to the project folder
-set PROJECT_PATH="D:/PAG/client_pag_exploratory"
+set PROJECT_PATH=D:/PAG/client_pag_exploratory
 
 rem Set scenario year
 set YEAR=2035
 
 rem Set scenario name
 set SCENARIO_NAME=test
+
+rem Set input excel file path
+set INPUT_EXCEL_FILE_PATH=2035_moves4_test.xlsx
+
+rem Set data interim folde directory
+set DATA_DIR="%PROJECT_PATH%/data/interim/"
+
+set DATABASE_PASSWORD=1234
+
 cd /d %PROJECT_PATH%/src
 
 rem Set the path to the R script to execute
-set RSCRIPT_FILE="write_moves_input_database_xml.R"
+set RSCRIPT_FILE=write_moves_input_database_xml.R
 
 rem Execute the R script
-%RSCRIPT% %RSCRIPT_FILE%
-
+%RSCRIPT% %RSCRIPT_FILE% %YEAR% %SCENARIO_NAME% %INPUT_EXCEL_FILE_PATH% %DATA_DIR%
 rem Set the path to the R script to execute
-set RSCRIPT_FILE="write_moves_run_spec.R"
+set RSCRIPT_FILE=write_moves_run_spec.R
 
 rem Execute the R script
-%RSCRIPT% %RSCRIPT_FILE%
+%RSCRIPT% %RSCRIPT_FILE% %YEAR% %SCENARIO_NAME% %DATA_DIR%
 
-::set RunSpecDir=%PROJECT_PATH%/src
+set RunSpecDir=%PROJECT_PATH%/src
 ::
 ::set MOVESDir=C:\Users\Public\EPA\MOVES\MOVES4.0
 ::
@@ -35,10 +43,10 @@ rem Execute the R script
 ::call ant run -Drunspec="%RunSpecDir%\%YEAR%_moves4_%SCENARIO_NAME%.mrs"
 ::
 ::rem Set the path to the R script to execute
-::set RSCRIPT_FILE="process_moves_outputs.R"
+set RSCRIPT_FILE="process_moves_outputs.R"
 ::
 ::rem Execute the R script
-::%RSCRIPT% %RSCRIPT_FILE%
+%RSCRIPT% %RSCRIPT_FILE% %YEAR% %SCENARIO_NAME% %DATA_DIR% %DATABASE_PASSWORD%
 
 cd /d %RunSpecDir%
 
