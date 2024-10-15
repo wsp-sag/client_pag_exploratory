f8740f0301c8c297e2ce430b7c0390822eb7526f
Arash <39972246+arashasadabadi@users.noreply.github.com>
2024-10-15 12:18:20 -0700

    Modify script so user can update Moves inputs for different years

--- src/copy_outputs_to_excel.py
+++ src/copy_outputs_to_excel.py
@@ -3,7 +3,7 @@ import sys
 import openpyxl
 import pandas as pd
 
-def main(excel_file, out_dir, scen_year):
+def main(excel_file, moves_input_dir, out_dir, scen_year):
 
     # read csv output files and process them to the right format for excel spredsheets
     vmt_df = pd.read_csv(os.path.join(out_dir, "output_annualVMTbySrcType.csv"))
@@ -14,10 +14,26 @@ def main(excel_file, out_dir, scen_year):
     vmt_df = vmt_df.rename(columns={'VehType': 'HPMSVtypeID', 'AnnualVMT': 'HPMSBaseYearVMT'})
     vmt_df = vmt_df[['HPMSVtypeID', 'yearID', 'HPMSBaseYearVMT']]
 
+    # read yearly MOVES inputs
+    age_distribution_df = pd.read_csv(os.path.join(moves_input_dir,  "ageDistribution_" + str(scen_year) + ".csv"))
+    avft_df = pd.read_csv(os.path.join(moves_input_dir,  "avft_" + str(scen_year) + ".csv"))
+    sourceTypePopulation_df = pd.read_csv(os.path.join(moves_input_dir,  "sourceTypePopulation_" + str(scen_year) + ".csv"))
+    fuel_supply_df = pd.read_csv(os.path.join(moves_input_dir,  "fuel_MOVES4defaultPimaCounty" + str(scen_year) + ".csv"))
+    fuel_formation_df = pd.read_csv(os.path.join(moves_input_dir,  "fuel_MOVES4defaultPimaCounty" + str(scen_year) + "_FuelFormulation.csv"))
+    fuel_usage_df = pd.read_csv(os.path.join(moves_input_dir,  "fuel_MOVES4defaultPimaCounty" + str(scen_year) + "_FuelUsageFraction.csv"))
+    inspection_maintenance_df = pd.read_csv(os.path.join(moves_input_dir,  "inspectionMaintenance_MOVES4defaultPimaCounty" + str(scen_year) + ".csv"))
+
     excel_sheets_mapping = [
         (vmt_df, 'HPMSannualVMT'),
         (roadtype_df, 'roadTypeDistribution'),
-        (vht_df, 'speedDistribution')
+        (vht_df, 'speedDistribution'),
+        (age_distribution_df, 'ageDistribution'),
+        (avft_df, 'avft'),
+        (sourceTypePopulation_df, 'sourceTypePopulation'),
+        (fuel_supply_df, 'fuelSupply'),
+        (fuel_formation_df, 'fuelFormulation'),
+        (fuel_usage_df, 'fuelUsageFraction'),
+        (inspection_maintenance_df, 'inspectionMaintenance')        
     ]
 
     workbook = openpyxl.load_workbook(excel_file)
@@ -36,14 +52,14 @@ def main(excel_file, out_dir, scen_year):
         
     workbook.save(excel_file)
 
-
 if __name__ == "__main__":
 
     args = sys.argv
     print(args)
     
     moves_excel_spreadsheet = args[1]  
-    output_directory = args[2]
-    scenario_year = int(args[3])
+    moves_input_path = args[2]
+    output_directory = args[3]
+    scenario_year = int(args[4])
     
-    main(moves_excel_spreadsheet, output_directory, scenario_year)
+    main(moves_excel_spreadsheet, moves_input_path, output_directory, scenario_year)
